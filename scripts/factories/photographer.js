e22f669b3a107e594d40b90c7036d378552f7649
Sonya-BM <58855475+Sonya-BM@users.noreply.github.com>
2022-10-19 11:53:21 +0200

    Add files via upload

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait, price, id, city, country, tagline } = data;
+    const { name, portrait, price, city, country, tagline } = data;
 
     const picture = `assets/photographers/Photographers_Photos/${portrait}`;
 
@@ -9,26 +9,23 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)   /*Définit la valeur d'un attribut sur l'élément spécifié. Si l'attribut existe déjà, la valeur est mise à jour ; sinon, un nouvel attribut est ajouté avec le nom et la valeur spécifiés. */
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        const num = document.createElement( 'p' );
-        num.textContent = id;
         const town = document.createElement( 'p' );
         town.textContent = city;
         const pays = document.createElement( 'p' );
-        pays.textContent = country;
+        pays.textContent = city+', '+country;
         const status = document.createElement( 'p' );
         status.textContent = tagline;
         const tarif = document.createElement( 'p' );
         tarif.textContent = price;
         article.appendChild(img); /*appendchild va générer des données*/
         article.appendChild(h2);
-        article.appendChild(num);
         article.appendChild(town);
-        article.appendChild(tarif);
         article.appendChild(pays);
+        article.appendChild(tarif);
         article.appendChild(status);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
 
 
