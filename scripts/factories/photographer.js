3fb50d76ba0f61e94b123708d5d863558a0ab033
Sonya-BM <58855475+Sonya-BM@users.noreply.github.com>
2022-10-28 14:56:17 +0200

    Add files via upload

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,24 @@
 function photographerFactory(data) {
+  const { name, portrait, price, city, country, tagline } = data;
+
+  const picture = `assets/photographers/Photographers_Photos/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    article.innerHTML = `
+       <img src="${picture}">
+       <h2>${name}</h2>
+       <p class="pays">${city + ", " + country}</p>
+       <p class="tagline"> ${tagline}</p>
+       <p class="tarif"> ${price} €/jour</p>
+       `;
+    return article;
+  }
+  return {getUserCardDOM };
+}
+
+/*
+function photographerFactory(data) {
     const { name, portrait, price, city, country, tagline } = data;
 
     const picture = `assets/photographers/Photographers_Photos/${portrait}`;
@@ -7,8 +27,8 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("alt", ""); /*ajout de l'attribut alt à l'image, le alt doit être vide comme ça nous a été demandé*/
-        img.setAttribute("src", picture)   /*Définit la valeur d'un attribut sur l'élément spécifié. Si l'attribut existe déjà, la valeur est mise à jour ; sinon, un nouvel attribut est ajouté avec le nom et la valeur spécifiés. */
-        const h2 = document.createElement( 'h2' );
+/*    img.setAttribute("src", picture)   /*Définit la valeur d'un attribut sur l'élément spécifié. Si l'attribut existe déjà, la valeur est mise à jour ; sinon, un nouvel attribut est ajouté avec le nom et la valeur spécifiés. */
+/*   const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const pays = document.createElement( 'p' ); 
         pays.classList.add("pays");
@@ -19,8 +39,8 @@ function photographerFactory(data) {
         const tarif = document.createElement( 'p' );
         tarif.classList.add("tarif");
         tarif.textContent = price + "€/jour";
-        article.appendChild(img); /*appendchild va générer des données*/
-        article.appendChild(h2);
+        article.appendChild(img); /*appendchild va créer html*/
+/* article.appendChild(h2);
         article.appendChild(pays);
         article.appendChild(status);
         article.appendChild(tarif);
@@ -30,22 +50,4 @@ function photographerFactory(data) {
 }
 
 
-/*
-function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/Photographers_Photos/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
-}
 */
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,17 @@
-const modalBg = document.querySelector("modal");
 
 function displayModal() {
-	modalBg.style.display = "block";
+const modal = document.getElementById("contact_modal");
+modal.style.display = "block"; 
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+const closeBtn = document.getElementById("close");
+const modalClose = document.getElementById("contact_modal");
+
+function close() {
+    modalClose.style.display = "none";
+    document
+        .getElementById("form_modal")
+        .reset(); //reset fonctionne qu'avec la balise form
+        
 }
+closeBtn.addEventListener("click", close);
