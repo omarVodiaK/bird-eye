ba96cf98d46b42b6cad9c3cafb6681f21a90d5b9
Sonya-BM <58855475+Sonya-BM@users.noreply.github.com>
2022-10-31 13:49:20 +0100

    Add files via upload

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,19 @@
 function photographerFactory(data) {
-  const { name, portrait, price, city, country, tagline } = data;
+  const { id,name, portrait, price, city, country, tagline } = data;
 
   const picture = `assets/photographers/Photographers_Photos/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
     article.innerHTML = `
-       <img src="${picture}">
-       <h2>${name}</h2>
-       <p class="pays">${city + ", " + country}</p>
-       <p class="tagline"> ${tagline}</p>
-       <p class="tarif"> ${price} €/jour</p>
+   <a href="./././photographer.html?${id}"> 
+   <img src="${picture}">
+   <h2>${name}</h2>
+   </a>   
+   <p class="pays">${city + ", " + country}</p>
+   <p class="tagline"> ${tagline}</p>
+   <p class="tarif"> ${price} €/jour</p>
+  
        `;
     return article;
   }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,21 @@
 //Mettre le code JavaScript lié à la page photographer.html
+function photographerFactory(data) {
+    const {name, portrait, price, city, country, tagline } = data;
+  
+    const picture = `assets/photographers/Photographers_Photos/${portrait}`;
+  
+    function getUserCardDOM() {
+      const article = document.createElement("article");
+      article.innerHTML = `
+     <img src="${picture}">
+     <h2>${name}</h2>
+     <p class="pays">${city + ", " + country}</p>
+     <p class="tagline"> ${tagline}</p>
+     <p class="tarif"> ${price} €/jour</p>
+    
+         `;
+      return article;
+    }
+    return {getUserCardDOM };
+  }
+  
